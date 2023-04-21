733aa5856d4cca171658774039a9940a46722cca
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-04-21 14:22:23 -0300

    Add arquivos do projeto Primefaces

---
+++ src/main/java/br/com/fiap/progamer/dao/SetupDao.java
@@ -0,0 +1,33 @@
+package br.com.fiap.progamer.dao;
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import br.com.fiap.progamer.model.SetupModel;
+
+@Named
+public class SetupDao {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public SetupDao(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@Transactional
+	public void save(SetupModel setupModel) {
+		this.entityManager.merge(setupModel);
+	}
+	
+	public List<SetupModel> findAll(){
+		@SuppressWarnings("unchecked")
+		TypedQuery<SetupModel> query = (TypedQuery<SetupModel>) entityManager.createQuery("SELECT e FROM SetupModel e");
+		return query.getResultList();
+	}
+	
+}
